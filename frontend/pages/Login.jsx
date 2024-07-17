8aa8f2ad3a5a3f8e73acc00a23dc7b45d82aa205
neeraj5696 <neerajkumar5696@gmail.com>
2024-07-17 18:33:52 +0530

    authentication fixed

--- frontend/pages/Login.jsx
+++ frontend/pages/Login.jsx
@@ -1,25 +1,47 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { useNavigate } from "react-router-dom";
-import '../pages/Signup.css'
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../utility/firebase";
+import '../pages/Signup.css';
+
+
 
 function Login() {
   const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try { 
+      await signInWithEmailAndPassword(auth, email, password)
+      console.log("user logged in Sucessfullly")
+      
+    } catch (error) {
+      console.log("error occured")
+      
+    }
+  }
 
   return (
     <div className="form">
   
 
-      <form>
+      <form onSubmit={handleSubmit} >
         <div className="input-container">
           <input
             type="text"
             name="uname"
             required
             placeholder="Email Address"
+            value={email}
+            onChange={(e)=> setEmail(e.target.value)}
           />
         </div>
         <div className="input-container">
-          <input type="password" name="pass" required placeholder="Password" />
+          <input type="password" name="pass" required placeholder="Password" value={password}
+            onChange={(e)=> setPassword(e.target.value)} />
         </div>
         <div className="button-container">
          <button type="submit">Login to your account</button>
--- frontend/pages/Signup.jsx
+++ frontend/pages/Signup.jsx
@@ -1,27 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../utility/firebase";
 import "../pages/Signup.css";
 
 function Signup() {
   const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [rpassword, setRpassword] = useState("");
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+      const user = auth.currentUser;
+      console.log(user);
+      console.log("registered successfully!!");
+    } catch (error) {
+      console.log("error occurred");
+    }
+  };
+
   return (
     <div className="form-container">
-      <form className="form">
+      <form onSubmit={handleRegister} className="form">
         <div className="input-container">
-          <input type="text" name="uname" placeholder="Email Address" />
+          <input
+            type="text"
+            placeholder="Email Address"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
         </div>
         <div className="input-container">
-          <input type="password" name="pass" placeholder="Password" />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
         </div>
         <div className="input-container">
-          <input type="password" name="repeatPass" placeholder="Repeat Password" />
+          <input
+            type="password"
+            placeholder="Repeat Password"
+            value={rpassword}
+            onChange={(e) => setRpassword(e.target.value)}
+            required
+          />
         </div>
         <div className="button-container">
           <button type="submit">Create an account</button>
         </div>
         <div className="account-message">
-          Already have an account? 
-          <button className="login-button" onClick={() => navigate("/login")} type="button">
+          Already have an account?
+          <button
+            className="login-button"
+            onClick={() => navigate("/login")}
+            type="button"
+          >
             Login
           </button>
         </div>
