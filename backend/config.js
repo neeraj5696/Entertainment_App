86b4b2041e9d3d13215ca01cb61539b8e2ddbe88
neeraj5696 <neerajkumar5696@gmail.com>
2024-07-25 11:39:37 +0530

    final

--- backend/config.js
+++ backend/config.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/authApp", {
+    await mongoose.connect("mongodb://localhost:27017/local", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
